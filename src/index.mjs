8bc55f24f49bfe19a1ba36d6c794ecd37da4ac66
solaaahhh <mnsolahuddin@gmail.com>
2025-03-01 10:24:56 +0800

    Add middleware

--- src/index.mjs
+++ src/index.mjs
@@ -2,17 +2,34 @@ import { log } from "console";
 import express from "express"
 
 const app = express();
+app.use(express.json());
 
-const APP_PORT = process.env.APP_PORT || 3000;
+const APP_PORT = process.env.APP_PORT;
 
 let users = [
     { id: 1, name: "John" },
     { id: 2, name: "Wock" }
 ];
 
+const loggingMiddleware = (request, response, next) => {
+    console.log(request.method + "-" + request.path);
+
+    next();
+}
+
+const validateUserId = (request, response, next) => {
+    const { params: { id } } = request;
+
+    if (isNaN(id)) return response.status(422).send({"message": "Invalid id"});
+    
+    next();
+}
+
 app.listen(APP_PORT, () => {
     console.log(`Running on port ${APP_PORT}`);
 });
+
+app.use(loggingMiddleware);
 
 app.get("/", (request, response) => {
     response.send("Hello world!")
@@ -22,21 +39,21 @@ app.get("/api/users", (request, response) => {
     const { query: { filter, value } } = request;
     
     if (filter && value) {
+
         let filteredusers = users.filter((user) => user[filter].includes(value));
+
         return response.send(filteredusers);
+
     }
     
     return response.send(users);
 });
 
-app.patch("/api/users/:id", (request, response) => {
+app.patch("/api/users/:id", validateUserId, (request, response) => {
     let user = users.find((user) => user.id == request.params.id);
+
     const id = parseInt(request.params.id);
-    if (isNaN(id)) {
-        return response.status(422).send({"message": "Invalid id"});
-    }
-    if (! user) {
-        return response.status(404).send({"message": "Not found"});
-    }
+
     return response.send(user);
-})
+});
+

851516cf3fbf9c252c6b6406a609f671f610d386
solaaahhh <mnsolahuddin@gmail.com>
2025-03-03 20:44:26 +0800

    Install mariadb

---
+++ src/databases/database.mjs
@@ -0,0 +1,10 @@
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'commerce'
+});
+
+export default pool;
